ec6b4f96757230ced8c771061872923ea6a8a1eb
Philipp Steiner <philipp-steiner@gmx.de>
2021-10-26 10:34:52 +0200

    little changes in codeflow

--- backend/src/main/java/de/neuefische/devquiz/security/config/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/devquiz/security/config/SecurityConfig.java
@@ -1,19 +1,16 @@
 package de.neuefische.devquiz.security.config;
 
-
 import de.neuefische.devquiz.security.service.AppUserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 import javax.servlet.Filter;
 
 @EnableWebSecurity
